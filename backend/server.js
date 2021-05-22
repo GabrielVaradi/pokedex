f50adf5db0f5ca84874326d9cd7981ea7d196ef1
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2021-05-22 13:07:23 -0400

    Remove unnecessary files and fetch data from api

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,43 @@
-const express = require('express');
-// const SocketServer = require('ws').Server;
-// const axios = require('axios')
-// const fetch = require('node-fetch');
-const path = require("path")
-// require('dotenv').config()
+const express = require("express");
+const cors = require("cors")
+const fetch = require("node-fetch");
+// require("dotenv").config()
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = app.use(express.static('../client/public')).listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(cors())
+app.use("/api", express.static("../client/public")).listen(PORT, "0.0.0.0", "localhost", () => console.log(`Listening on ${ PORT }`));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join('../client/public'));
-});
 
-// const wss = new SocketServer({
-//     server
-// }); 
+fetchPokemons = async () => {
+  const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon/1")
+  .then(res => res.json())
+  .catch((error => {
+    console.error("Error: " + error)
+  }))
   
-// wss.on('connection', (ws) => {
-//     console.log('Client connected');
-//     ws.on('close', () => console.log('Client disconnected'));
-// });
-  
-
-
-fetchData = async () => {
-    // if (!getToken) {
-    //     getToken = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.REACT_APP_TWITCH_API_CLIENT_ID}&client_secret=${process.env.REACT_APP_TWITCH_API_CLIENT_SECRET}&grant_type=client_credentials&scope=analytics:read:games`)
-    //     .then(response => response)
-    //     .catch(e => console.log(e))
-    // } else {
-    //     setInterval(async () => {
-    //         getToken = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.REACT_APP_TWITCH_API_CLIENT_ID}&client_secret=${process.env.REACT_APP_TWITCH_API_CLIENT_SECRET}&grant_type=client_credentials&scope=analytics:read:games`)
-    //         .then(response => response)
-    //         .catch(e => console.log(e))
-    //     },  12600000);
-    // }
-
-    // const data = await Promise.all(series.map(serie => {
-    //     return fetch(serie.url, {
-    //         "headers": {
-    //                 "Client-ID": process.env.REACT_APP_TWITCH_API_CLIENT_ID,
-    //                 "Authorization": "Bearer " + getToken.data.access_token
-    //         }
-    //     }).then(resp => resp.json())}
-    //     )).catch(e => console.log(e))
+  return pokemons
 }
+
+fetchPokemons()
+  .then(pokemons => {
+    app.get("/", (req, res) => {
+      res.send(pokemons);
+    });
+  })
+
+
+
+
+
+// app.get("/api", (req, res) => {
+//   res.sendFile("../client/public");
+// });
+
+  
+
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class  App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: ""
+    }
+  }
+
+  componentDidMount() {
+    this.callApi()
+
+  }
+
+  callApi = () => {
+    fetch("http://localhost:3001")
+        .then(res => res.json())
+        .then(res => this.setState({ pokemons: res }));
+  }
+
+  render() {
+    console.log(this.state)
+    const { pokemons } = this.state.pokemons
+    if (this.state.pokemons) {
+      return (
+        <div className="App">
+          {pokemons.name}
+          <img src={pokemons.sprites.front_default} alt={pokemons.name}/>
+        </div>
+      )
+    } else {
+      return ""
+    }
+  }
 }
 
 export default App;
